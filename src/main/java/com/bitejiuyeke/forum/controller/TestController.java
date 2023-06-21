b59ceaefddf379731ed568517d30cda3b56a4375
yangbiao <3053999187@qq.com>
2023-06-21 10:53:33 +0800

    第一次提交

---
+++ src/main/java/com/bitejiuyeke/forum/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.bitejiuyeke.forum.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 比特就业课
+ */
+// 表示返回的结果是数据
+@RestController
+// 定义一级映射路径
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello () {
+        return "hello, Spring boot...";
+    }
+}
